d5e2be2640fe30e6ff76131cf1b72b5548245bbc
Payal Sahu <141853271+payalsahu1303@users.noreply.github.com>
2026-01-12 09:42:28 +0530

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,46 +1,32 @@
 public class Student {
+    int id;
+    String name;
+    int age;
+    String course;
 
-    private int id;
-    private String name;
-    private int age;
-    private String course;
-
-    public Student(int id, String name, int age, String course) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.course = course;
+    public Student(int i, String n, int a, String c) {
+        id = i;
+        name = n;
+        age = a;
+        course = c;
     }
 
     public int getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public void setName(String n) {
+        name = n;
     }
 
-    public int getAge() {
-        return age;
+    public void setAge(int a) {
+        age = a;
     }
 
-    public String getCourse() {
-        return course;
+    public void setCourse(String c) {
+        course = c;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
-    @Override
     public String toString() {
         return id + " | " + name + " | " + age + " | " + course;
     }
